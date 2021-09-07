901bf9fffb9e9892dbf1e40db1a8b0f839d15fb4
Alessandro dos Santos <professor_alessandro@hotmail.com>
2021-09-07 09:51:11 -0300

    updated procedure logspaginated

--- PROCEDURES/ProceduresAPDBDev.sql
+++ PROCEDURES/ProceduresAPDBDev.sql
@@ -19,7 +19,14 @@ CREATE PROCEDURE [dbo].[TiposTelefonesPaginated]
 			-- SET @PageNumber = 2
 			-- SET @RowspPage = 5
 
-			SELECT 		*
+			SELECT
+				[TipoTelefoneId]
+				,[Descricao]
+				,[AssociadoInclusaoId]
+				,[AssociadoUltimaAlteracaoId]
+				,[DataInclusao]
+				,[DataUltimaAlteracao]
+				,[Ativo]
 			FROM 		[TiposTelefones]
 			WHERE 		([TipoTelefoneId] = @Id OR @Id IS NULL)
 			AND 		([Descricao] LIKE '%'+@Descricao+'%' OR @Descricao IS NULL)
@@ -53,7 +60,14 @@ GO
 			-- SET @PageNumber = 2
 			-- SET @RowspPage = 5
 
-			SELECT 		*
+			SELECT
+				[TipoDocumentoId]
+				,[Descricao]
+				,[AssociadoInclusaoId]
+				,[AssociadoUltimaAlteracaoId]
+				,[DataInclusao]
+				,[DataUltimaAlteracao]
+				,[Ativo]
 			FROM 		[TiposDocumentos]
 			WHERE 		([TipoDocumentoId] = @Id OR @Id IS NULL)
 			AND 		([Descricao] LIKE '%'+@Descricao+'%' OR @Descricao IS NULL)
@@ -87,7 +101,14 @@ GO
 			-- SET @PageNumber = 2
 			-- SET @RowspPage = 5
 
-			SELECT 		*
+			SELECT
+				[TipoConfiguracaoId]
+				,[Descricao]
+				,[AssociadoInclusaoId]
+				,[AssociadoUltimaAlteracaoId]
+				,[DataInclusao]
+				,[DataUltimaAlteracao]
+				,[Ativo]
 			FROM 		[TiposConfiguracoes]
 			WHERE 		([TipoConfiguracaoId] = @Id OR @Id IS NULL)
 			AND 		([Descricao] LIKE '%'+@Descricao+'%' OR @Descricao IS NULL)
@@ -121,7 +142,14 @@ GO
 			-- SET @PageNumber = 2
 			-- SET @RowspPage = 5
 
-			SELECT 		*
+			SELECT
+				[TipoLancamentoId]
+				,[Descricao]
+				,[AssociadoInclusaoId]
+				,[AssociadoUltimaAlteracaoId]
+				,[DataInclusao]
+				,[DataUltimaAlteracao]
+				,[Ativo]
 			FROM 		[TiposLancamentos]
 			WHERE 		([TipoLancamentoId] = @Id OR @Id IS NULL)
 			AND 		([Descricao] LIKE '%'+@Descricao+'%' OR @Descricao IS NULL)
@@ -155,7 +183,14 @@ GO
 			-- SET @PageNumber = 2
 			-- SET @RowspPage = 5
 
-			SELECT 		*
+			SELECT
+				[TipoParametroId]
+				,[Descricao]
+				,[AssociadoInclusaoId]
+				,[AssociadoUltimaAlteracaoId]
+				,[DataInclusao]
+				,[DataUltimaAlteracao]
+				,[Ativo]
 			FROM 		[TiposParametros]
 			WHERE 		([TipoParametroId] = @Id OR @Id IS NULL)
 			AND 		([Descricao] LIKE '%'+@Descricao+'%' OR @Descricao IS NULL)
@@ -189,7 +224,14 @@ GO
 			-- SET @PageNumber = 2
 			-- SET @RowspPage = 5
 
-			SELECT 		*
+			SELECT
+				[TipoWorkFlowId]
+				,[Descricao]
+				,[AssociadoInclusaoId]
+				,[AssociadoUltimaAlteracaoId]
+				,[DataInclusao]
+				,[DataUltimaAlteracao]
+				,[Ativo]
 			FROM 		[TiposWorkFlows]
 			WHERE 		([TipoWorkFlowId] = @Id OR @Id IS NULL)
 			AND 		([Descricao] LIKE '%'+@Descricao+'%' OR @Descricao IS NULL)
@@ -211,7 +253,7 @@ GO
 	-- CREATING A PAGING WITH OFFSET and FETCH clauses IN "SQL SERVER 2012"
 	-- CREATED BY ALESSANDRO 08/05/2021
 	-- THIS PROCEDURE RETURNS TABLE TIPOS TELEFONED PAGINATED
-	CREATE PROCEDURE [dbo].[TipoDadosPaginated]
+	CREATE PROCEDURE [dbo].[TiposDadosPaginated]
 		@Id INT,
 		@Descricao VARCHAR(MAX),
 		@Ativo BIT,
@@ -223,11 +265,67 @@ GO
 			-- SET @PageNumber = 2
 			-- SET @RowspPage = 5
 
-			SELECT 		*
+			SELECT
+				[TipoDadoId]
+				,[Descricao]
+				,[AssociadoInclusaoId]
+				,[AssociadoUltimaAlteracaoId]
+				,[DataInclusao]
+				,[DataUltimaAlteracao]
+				,[Ativo]
 			FROM 		[TiposDados]
 			WHERE 		([TipoDadoId] = @Id OR @Id IS NULL)
 			AND 		([Descricao] LIKE '%'+@Descricao+'%' OR @Descricao IS NULL)
 			AND			([Ativo] = @Ativo OR @Ativo IS NULL)
+			ORDER BY 1 DESC
+			OFFSET ((@PageNumber - 1) * @RowspPage) ROWS
+			FETCH NEXT @RowspPage ROWS ONLY;
+		END
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- -----------------------------------------------------
+-- Procedure [log].[LogsPaginated]
+-- -----------------------------------------------------
+
+	-- CREATING A PAGING WITH OFFSET and FETCH clauses IN "SQL SERVER 2012"
+	-- CREATED BY ALESSANDRO 08/05/2021
+	-- THIS PROCEDURE RETURNS TABLE lLOGS PAGINATED
+	CREATE PROCEDURE [log].[LogsPaginated]
+		@Id INT,
+		@Message VARCHAR(MAX),
+		@Method VARCHAR(10),
+		@Request VARCHAR(150),
+		@Response INT,
+		@UserId INT,
+		@DateAdded DATETIME,
+		@PageNumber INT,
+		@RowspPage INT
+	AS
+		BEGIN
+			-- ATRIB TESTE PROC
+			-- SET @PageNumber = 2
+			-- SET @RowspPage = 5
+
+			SELECT
+				[LogId]
+				,[Message]
+				,[Request]
+				,[Method]
+				,[Response]
+				,[UserAddedId]
+				,[DateAdded]
+			FROM 		[log].[Logs]
+			WHERE 		([LogId] = @Id OR @Id IS NULL)
+			AND 		([Message] LIKE '%'+@Message+'%' OR @Message IS NULL)
+			AND 		([Method] LIKE '%'+@Method+'%' OR @Method IS NULL)
+			AND 		([Request] LIKE '%'+@Request+'%' OR @Request IS NULL)
+			AND 		([Response] = @Response OR @Response IS NULL)
+			AND 		([UserAddedId] = @UserId OR @UserId IS NULL)
+			AND 		([DateAdded] >= @DateAdded OR @DateAdded IS NULL)
 			ORDER BY 1 DESC
 			OFFSET ((@PageNumber - 1) * @RowspPage) ROWS
 			FETCH NEXT @RowspPage ROWS ONLY;
